8caf71a4db2d7d648ee78c5a5c317ebb575718b1
leoc3120 <58136950+leoc3120@users.noreply.github.com>
2021-11-04 13:49:36 +0100

    Add files via upload

---
+++ mvcCORV1/controle/inscription.php
@@ -0,0 +1,46 @@
+<?php 
+	/*controleur utilisateur.php :
+		fonctions-action de gestion des utilisateurs
+	*/
+	
+	function identification () {
+		$nom=isset($_POST['nom'])?trim($_POST['nom']):''; // trim pour enlever les espaces avant et apres
+		$prenom=isset($_POST['prenom'])?trim($_POST['prenom']):'';
+        $email=isset($_POST['email'])?trim($_POST['email']):'';
+        $num=isset($_POST['num'])?trim($_POST['num']):'';
+		$msg="";
+
+        require ("./modele/utilisateurBD.php");
+        if (!verif_bd($nom, $num, $profil)){
+            inscription($nom,$prenom,$email,$num);
+			echo "inscription réussi";
+        } else{
+            require("vue/utilisateur/identification.html");
+        }
+        
+	}
+    function pagep() {
+        $email=isset($_POST['email'])?trim($_POST['email']):'';
+        $num=isset($_POST['num'])?trim($_POST['num']):'';
+
+        
+		if (count($_POST)==0) require("vue/utilisateur/pagep.html");
+		else {
+			
+			require ("./modele/utilisateurBD.php");
+			
+			if (verif_bd($email, $num, $profil)) {
+				//echo ('<br/>PROFIL : <pre>'); var_dump ($profil); echo ('</pre><br/>'); die("ident");
+				
+				//session_start(); //deja fait dans index
+				$_SESSION['profil'] = $profil;
+				$nexturl = "index.php?controle=utilisateur&action=accueil";
+				header ("Location:" . $nexturl);
+			}
+			else {
+				$msg = "Utilisateur inconnu !";
+				require("vue/utilisateur/pagep.html");
+			}
+		}
+		
+	}
---
+++ mvcCORV1/controle/utilisateur.php
@@ -0,0 +1,74 @@
+﻿<?php 
+	/*controleur utilisateur.php :
+		fonctions-action de gestion des utilisateurs
+	*/
+	
+	function ident () {
+		$nom=isset($_POST['nom'])?trim($_POST['nom']):''; // trim pour enlever les espaces avant et apres
+		$num=isset($_POST['num'])?trim($_POST['num']):'';
+		$msg="";
+
+		if (count($_POST)==0) require("vue/utilisateur/ident.html");
+		else {
+			
+			require ("./modele/utilisateurBD.php");
+			
+			if (verif_bd($nom, $num, $profil)) {
+				//echo ('<br/>PROFIL : <pre>'); var_dump ($profil); echo ('</pre><br/>'); die("ident");
+				
+				//session_start(); //deja fait dans index
+				$_SESSION['profil'] = $profil;
+				$nexturl = "index.php?controle=utilisateur&action=accueil";
+				header ("Location:" . $nexturl);
+			}
+			else {
+				$msg = "Utilisateur inconnu !";
+				require("vue/utilisateur/ident.html");
+			}
+		}
+	}
+	function pagep() {
+		$email=isset($_POST['email'])?trim($_POST['email']):''; // trim pour enlever les espaces avant et apres
+		$num=isset($_POST['num'])?trim($_POST['num']):'';
+		$msg="";
+
+		if (count($_POST)==0) require("vue/utilisateur/pagep.html");
+		else {
+			
+			require ("./modele/utilisateurBD.php");
+			
+			if (verif_bd($email, $num, $profil)) {
+				//echo ('<br/>PROFIL : <pre>'); var_dump ($profil); echo ('</pre><br/>'); die("ident");
+				
+				//session_start(); //deja fait dans index
+				$_SESSION['profil'] = $profil;
+				$nexturl = "index.php?controle=utilisateur&action=accueil";
+				header ("Location:" . $nexturl);
+			}
+			else {
+				$msg = "Utilisateur inconnu !";
+				require("vue/utilisateur/pagep.html");
+			}
+		}
+	}
+	
+	function accueil() {
+		$nom=isset($_POST['nom'])?trim($_POST['nom']):''; 
+		$prenom=isset($_POST['prenom'])?trim($_POST['prenom']):''; 
+		require ("vue/utilisateur/accueil.html");
+	}
+	
+	function bye() {
+		echo ("<h2>Au revoir M. ou Mdme " . $_SESSION['profil']['nom'] . "</h2>");
+		session_destroy();
+	}
+	
+	function ajout_u()  {
+		echo ("ajout_u ::");
+	}
+	function maj_u() {
+		echo ("maj_u ::");
+	}
+	function destr_u ()  {
+		echo ("destr_u ::");
+	}				
